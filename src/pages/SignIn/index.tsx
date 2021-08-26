c35023bc1957f2efb190d9a26e8a2409f009db8c
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2021-08-26 15:40:16 +0100

    feat: implement input and button components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SigIn: React.FC = () => (
   <Container>
@@ -10,12 +13,9 @@ const SigIn: React.FC = () => (
 
       <form>
         <h1>Faça o seu Logon</h1>
-
-        <input placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-
-        <button type="submit">Entrar</button>
-
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+        <Button type="submit">Entrar</Button>
         <a href="forgot">Esqueci minha senha</a>
       </form>
 
@@ -23,6 +23,7 @@ const SigIn: React.FC = () => (
         <FiLogIn />
         Criar Conta
       </a>
+
     </Content>
     <Background />
   </Container>
