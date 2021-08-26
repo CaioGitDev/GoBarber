5e76e84e968fa2640cbc0090116cefe411f4780c
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2021-08-26 01:41:54 +0100

    feat: init sigin page

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import sigInBackgroundImg from '../../assets/sign-in-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  place-content: center;
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      background: #232129;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      padding: 16px;
+      width: 100%;
+
+      & + input {
+        margin-top: 8px;
+      }
+    }
+
+    button {
+      background: #ff9000;
+      height: 56px;
+      border-radius: 10px;
+      border: 0;
+      padding: 0 16px;
+      color: #312e38;
+      width: 100%;
+      font-weight: 500;
+      margin-top: 16px;
+    }
+  }
+`;
+export const Background = styled.div`
+  flex: 1;
+  background: url(${sigInBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
