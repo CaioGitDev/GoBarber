c236c8dd092ecccc24387092955b1eda852f0c81
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2021-08-26 01:41:41 +0100

    feat: initial config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SigIn from './pages/SignIn';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <SigIn />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
